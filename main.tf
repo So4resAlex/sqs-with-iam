5490351cfa9362468b40b6f89fca162644cb8cb7
Alex Soares <alex.soares@girotech.com.br>
2024-04-04 09:24:06 -0300

    feat: add main file from tests

--- main.tf
+++ main.tf
@@ -5,8 +5,7 @@ terraform {
         version = "5.37.0"
     }   
   }
-  backend "s3" {} #Init with 
-
+  backend "s3" {} #Init with -backend-config parameter 
 }
 provider "aws" {
   region = "us-east-1"
