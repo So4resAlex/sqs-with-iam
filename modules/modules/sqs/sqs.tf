28630d77dfe05997cdeeccc9e85b67d50c40d6ed
Alex Soares <alex.soares@girotech.com.br>
2024-04-03 21:09:00 -0300

    feat: add aws_sqs_policy

--- modules/modules/sqs/sqs.tf
+++ modules/modules/sqs/sqs.tf
@@ -9,3 +9,39 @@ resource "aws_sqs_queue" "terraform_queue" {
     Eviroment = "Test"
   }
 }
+
+resource "aws_sqs_queue_policy" "terraform_queue_policy" {
+  queue_url = aws_sqs_queue.terraform_queue.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = var.policy_sqs_name
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = var.receiver_user_arn #TODO Receive user arn
+        }
+        Action = [
+          "sqs:ChangeMessageVisibility",
+          "sqs:DeleteMessage",
+          "sqs:ReceiveMessage",
+          "sqs:GetQueueUrl",
+          "sqs:GetQueueAttributes"
+        ]
+        Resource = aws_sqs_queue.terraform_queue.arn
+      },
+      {
+        Effect = "Allow"
+        Principal = {
+          AWS = var.sender_user_arn #TODO Sender user arn
+        }
+        Action = [
+          "sqs:SendMessage",
+          "sqs:GetQueueUrl",
+          "sqs:GetQueueAttributes"
+        ]
+        Resource = aws_sqs_queue.terraform_queue.arn
+      }
+    ]
+  })
+}
