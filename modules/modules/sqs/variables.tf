a161742e0413d0a08786abddf947e1335ef656b0
Alex Soares <alex.soares@girotech.com.br>
2024-04-03 21:03:38 -0300

    feat: add variable from sqs module

---
+++ modules/modules/sqs/variables.tf
@@ -0,0 +1,19 @@
+variable "queue_name" {
+  description = "The name from queue to be created"
+  type = string
+}
+
+variable "policy_sqs_name" {
+  description = "The name from policy queue"
+  type = string
+}
+
+variable "user_arn" {
+  description = "User ARN for binding"
+  type = string
+}
+
+variable "enviroment" {
+    description = "Enviroment"
+    type = string
+}
