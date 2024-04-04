b82d299c63da1a862d667b17f7937820225545fa
Alex Soares <alex.soares@girotech.com.br>
2024-04-03 21:11:39 -0300

    feat: add sender and receiver user

--- modules/modules/sqs/variables.tf
+++ modules/modules/sqs/variables.tf
@@ -1,19 +1,24 @@
 variable "queue_name" {
   description = "The name from queue to be created"
-  type = string
+  type        = string
 }
 
 variable "policy_sqs_name" {
   description = "The name from policy queue"
-  type = string
+  type        = string
 }
 
 variable "user_arn" {
   description = "User ARN for binding"
-  type = string
+  type        = string
 }
 
 variable "enviroment" {
-    description = "Enviroment"
-    type = string
+  description = "Enviroment"
+  type        = string
+}
+
+variable "sender_user_arn" {
+  description = "The sender user from queue"
+  type        = string
 }
