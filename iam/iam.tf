c4cf340c13f4cca0e449682f1f0a5444557a4e36
Alex Soares <alex.soares@girotech.com.br>
2024-04-06 15:53:53 -0300

    chore: remove files

--- iam/iam.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_iam_user" "terraform_iam_user" {
-  name = var.iam_name_user
-  tags = {
-    Eviroment = "Test"
-  }
-}
-output "iam_arn" {
-  value = aws_iam_user.terraform_iam_user.arn
-}
