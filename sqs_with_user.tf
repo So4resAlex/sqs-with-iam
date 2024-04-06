6a9176e6c6275f5f5827767c979e61619fbbb372
Alex Soares <alex.soares@girotech.com.br>
2024-04-06 15:55:10 -0300

    refactor: add all resources and add count

---
+++ sqs_with_user.tf
@@ -0,0 +1,61 @@
+resource "aws_iam_user" "sender" {
+  count = var.create_sender ? 1 : 0
+  name = var.sender_user_name
+  tags = {
+    Enviroment = var.enviroment
+  }
+}
+
+resource "aws_iam_user" "receiver" {
+  count = var.create_receiver ? 1 : 0
+  name = var.receiver_user_name
+  tags = {
+    Enviroment = var.enviroment
+  }
+}
+
+resource "aws_sqs_queue" "terraform_queue" {
+  name                       = var.queue_name
+  delay_seconds              = 20
+  visibility_timeout_seconds = 60
+  receive_wait_time_seconds  = 20
+  message_retention_seconds  = 1209600
+  max_message_size           = 2048
+  tags = {
+    Environment = "Test"
+  }
+}
+
+resource "aws_sqs_queue_policy" "terraform_queue_policy" {
+  queue_url = aws_sqs_queue.terraform_queue.url
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = var.policy_sqs_name
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { AWS = var.create_receiver ? aws_iam_user.receiver[0].arn : var.receiver_user_arn }
+        Action    = [
+          "sqs:ChangeMessageVisibility",
+          "sqs:DeleteMessage",
+          "sqs:ReceiveMessage",
+          "sqs:GetQueueUrl",
+          "sqs:GetQueueAttributes"
+        ]
+        Resource  = aws_sqs_queue.terraform_queue.arn
+      },
+      {
+        Effect    = "Allow"
+        Principal = { AWS = var.create_sender ? aws_iam_user.sender[0].arn : var.sender_user_arn }
+        Action    = [
+          "sqs:SendMessage",
+          "sqs:GetQueueUrl",
+          "sqs:GetQueueAttributes"
+        ]
+        Resource  = aws_sqs_queue.terraform_queue.arn
+      }
+    ]
+  })
+}
+
