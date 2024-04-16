e3b390fcc99e46b4776b4cbc76ba7d8e070abd61
Alex Soares <alex.soares@girotech.com.br>
2024-04-16 20:08:59 -0300

    fix: adjust value

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,8 @@
 output "sender_user_arn" {
-  value = aws_iam_user.sender.arn
+  value = aws_iam_user.sender[0].arn
 }
 
 output "receiver_user_arn" {
-  value = aws_iam_user.receiver.arn
+  value = aws_iam_user.receiver[0].arn
 }
 
